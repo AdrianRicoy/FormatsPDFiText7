cdd73d2a9647a4b31ec63794a584fe4c7229cf5f
AdrianRicoy <noe.ricoy@hotmail.com>
2019-09-24 00:06:22 -0500

    Vista y controlador principal

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PdfHandler.Models;
+
+namespace PdfHandler.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["ListPdf"] = new HandlerPdf().PdfNames();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public ActionResult Index(string pdfName)
+        {
+            if(!string.IsNullOrEmpty(pdfName))
+            {
+                byte[] pdf = new HandlerPdf().StartHandlerPdf(pdfName.Split(": ")[1]);
+                HttpContext.Response.Headers.Add("Content-Disposition", "attachment; filename=prueba.pdf");
+                return File(pdf, "aplication/pdf");
+            }
+
+            return View();
+        }
+    }
+}
