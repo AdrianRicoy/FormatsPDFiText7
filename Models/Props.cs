181ae557015fea13d6eaa5e858d8cef87a9659f1
AdrianRicoy <noe.ricoy@hotmail.com>
2019-09-28 02:08:36 -0500

    Se ha creado el modelo Props como una clase para uso general

---
+++ Models/Props.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PdfHandler.Models
+{
+    public static class Props
+    {
+        /// <summary>
+        /// Genera un número aleatorio en un rango especificado
+        /// </summary>
+        /// <param name="range">Arreglo que contendra el rango</param>
+        /// <returns>El número aleatorio</returns>
+        public static int RandomNumber(int[] range)
+        {
+            return new Random().Next(range[0], range[1]);
+        }
+    }
+}
