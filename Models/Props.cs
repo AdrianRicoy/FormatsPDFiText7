eda86be30daafdc4e83822cfa0e939f32717a251
AdrianRicoy <noe.ricoy@hotmail.com>
2019-10-01 22:47:06 -0500

    Se ha cagregado el método GetFirstCapitalLetter para obtener la primera letra mayúscula de un texto.
    Y se ha cambiado los métodos de GetDayByDate para obtener el nombre
    en mayúscula dependiendo de lo que quiera el usuario
    
    Se ha agregado GetMonthByDatepara obtener el nombre de un mes

--- Models/Props.cs
+++ Models/Props.cs
@@ -22,10 +22,38 @@ namespace PdfHandler.Models
         /// </summary>
         /// <param name="date">Fecha con la que se obtendrá el día</param>
         /// <returns>Día</returns>
-        public static string GetDayByDate(DateTime date)
+        public static string GetDayByDate(DateTime date, bool firstCapitalLetter)
         {
             CultureInfo culture = new CultureInfo("Es-Es");
-            return culture.DateTimeFormat.GetDayName(DateTime.Now.DayOfWeek);
+            string day = culture.DateTimeFormat.GetDayName(DateTime.Now.DayOfWeek);
+
+            if (firstCapitalLetter) day = GetFirstCapitalLetter(day);
+
+            return day;
+        }
+        /// <summary>
+        /// Obtiene el nombre del mes de una fecha específica
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns>El nombre del mes</returns>
+        public static string GetMonthByDate(DateTime date, bool firstCapitalLetter)
+        {
+            DateTimeFormatInfo formatDate = CultureInfo.CurrentCulture.DateTimeFormat;
+            string month = formatDate.GetMonthName(date.Day);
+            
+            if(firstCapitalLetter) month = GetFirstCapitalLetter(month);
+            
+            return month;
+        }
+        /// <summary>
+        /// Genera el mismo texto que recibe pero con la primera letra en mayúscula
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>Texto con la primera letra mayúscula</returns>
+        public static string GetFirstCapitalLetter(string text)
+        {
+            TextInfo textInfo = new CultureInfo("Es-Es").TextInfo;
+            return textInfo.ToTitleCase(text);
         }
     }
 }
