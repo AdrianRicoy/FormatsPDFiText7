a2f4643ad1ad7a169df45e73fb3ba251d31007df
AdrianRicoy <noe.ricoy@hotmail.com>
2019-10-06 19:15:39 -0500

    Se agregó el método CreateMyPdf para el segundo formato simple con alineaciones

--- Models/SimpleFormatReport.cs
+++ Models/SimpleFormatReport.cs
@@ -112,6 +112,12 @@ namespace PdfHandler.Models
                 case 3:
                     paragraph.Add(new Text(text).SetFont(FontHelveticaNegrita).SetFontSize(fontSize+2)).SetTextAlignment(TextAlignment.CENTER);
                     break;
+                case 4:
+                    paragraph.Add(new Text(text).SetFont(FontHelvetica).SetFontSize(fontSize)).SetTextAlignment(TextAlignment.CENTER);
+                    break;
+                case 5:
+                    paragraph.Add(new Text(text).SetFont(FontHelvetica).SetFontSize(fontSize)).SetTextAlignment(TextAlignment.RIGHT);
+                    break;
             }
 
             return paragraph;
@@ -143,6 +149,12 @@ namespace PdfHandler.Models
                 case 3:
                     paragraph.Add(new Text(text).SetFont(FontHelveticaNegrita).SetFontSize(fontSize + 2)).SetTextAlignment(TextAlignment.CENTER);
                     break;
+                case 4:
+                    paragraph.Add(new Text(text).SetFont(FontHelvetica).SetFontSize(fontSize)).SetTextAlignment(TextAlignment.CENTER);
+                    break;
+                case 5:
+                    paragraph.Add(new Text(text).SetFont(FontHelvetica).SetFontSize(fontSize)).SetTextAlignment(TextAlignment.RIGHT);
+                    break;
             }
 
             return paragraph;
@@ -162,10 +174,13 @@ namespace PdfHandler.Models
 
             string fecha = string.Format("\n\nDay {0} of {1} - {2}", DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
 
+            #region header
             headerMessage.Add(fecha, 1);
             headerMessage.Add("Asunto: Prueba del formato simple", 2);
             headerMessage.Add("Probando Formato", 0);
+            #endregion
 
+            #region body
             bodyMessage.Add("\n\nEl universo tiene un principio, pero no un final. Infinito. Las estrellas también tienen un principio, pero su propio poder las conduce a su destrucción." +
                             "\nFinito.La historia nos ha enseñado que los más sabios son los más estúpidos." +
                             "\nEsto podría llamarse La última advertencia de Dios a aquellos que todavía resisten.", 1);
@@ -173,9 +188,54 @@ namespace PdfHandler.Models
 
             bodyMessage.Add("\n\nSoy una científica, mis acciones se basan en la lógica y en los números, jamás me dejaría llevar por mis sentimientos.", 1);
             bodyMessage.Add("\n- Kurisu Makise", 2);
+            #endregion
 
+            #region footer
             footerMessage.Add("\n\nTexto al Azar", 3);
+            #endregion
 
+
+            return Generateformat(headerMessage, bodyMessage, footerMessage);
+        }
+        /// <summary>
+        /// Generar el formato simple PDF con distintas alineaciones
+        /// </summary>
+        /// <returns>Un arreglo en byte con la información del PDF</returns>
+        public byte[] CreateMyPdf2()
+        {
+            Dictionary<string, int> headerMessage = new Dictionary<string, int>();
+            Dictionary<string, int> bodyMessage = new Dictionary<string, int>();
+            Dictionary<string, int> footerMessage = new Dictionary<string, int>();
+
+            string fecha = string.Format("\n\nDay {0} of {1} - {2}", DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
+
+            #region header
+            headerMessage.Add(fecha, 1);
+            headerMessage.Add("Asunto: Prueba del formato simple", 2);
+            headerMessage.Add("Probando Formato", 0);
+            #endregion
+
+            #region body
+            bodyMessage.Add("\n\nEl universo tiene un principio, pero no un final. Infinito. Las estrellas también tienen un principio, pero su propio poder las conduce a su destrucción." , 1);
+            bodyMessage.Add("\nFinito.La historia nos ha enseñado que los más sabios son los más estúpidos.", 2);
+
+            bodyMessage.Add("\nEsto podría llamarse La última advertencia de Dios a aquellos que todavía resisten.", 5);
+
+            bodyMessage.Add("- Okabe Rintaro", 4);
+
+            bodyMessage.Add("\n\nSoy una científica, mis acciones se basan en la lógica y en los números, jamás me dejaría llevar por mis sentimientos.", 0);
+            bodyMessage.Add("\n- Kurisu Makise", 3);
+            #endregion
+
+            #region footer
+            footerMessage.Add("________________________________________________________________", 3);
+            footerMessage.Add("Texto al Azar 0", 0);
+            footerMessage.Add("Texto al Azar 1", 1);
+            footerMessage.Add("Texto al Azar 2", 2);
+            footerMessage.Add("Texto al Azar 3", 3);
+            footerMessage.Add("Texto al Azar 4", 4);
+            footerMessage.Add("Texto al Azar 5", 5);
+            #endregion
 
             return Generateformat(headerMessage, bodyMessage, footerMessage);
         }
