2837c4b9435d39512ab483b1b8904540848fee08
AdrianRicoy <noe.ricoy@hotmail.com>
2019-10-09 22:15:08 -0500

    Se ha creado la clase Message, para mandar mensaje de error ó confirmación a la vista

---
+++ Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PdfHandler.Models
+{
+    public static class Message
+    {
+        public static string pdfNotFound = "El pdf seleccionado de momento no esta disponible";
+    }
+}
