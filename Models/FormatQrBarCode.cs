b7097915f77e06efcc055da18c0911c97dc06be5
AdrianRicoy <noe.ricoy@hotmail.com>
2019-10-09 23:31:30 -0500

    Se ha agregado los métodos GetBarcode25 y GetBarcodeEan

--- Models/FormatQrBarCode.cs
+++ Models/FormatQrBarCode.cs
@@ -32,11 +32,12 @@ namespace PdfHandler.Models
             #endregion
 
             #region QR
-            document.Add(GetQr(GetMenssageForQr()));
+            document.Add(GetQr(GetMenssageForBarcodes()));
             #endregion
 
-            #region Barcode
-
+            #region Barcodes
+            document.Add(GetBarcode25(pdf));
+            document.Add(GetBarcodeEan(pdf));
             #endregion
 
             document.Close();
@@ -56,24 +57,72 @@ namespace PdfHandler.Models
         }
         #endregion
 
-        #region Methods - Qr
-        private string GetMenssageForQr()
+        #region Methods - Barcodes
+        /// <summary>
+        /// Obtener mensaje que será convertido en los distintos Barcodes disponibles
+        /// </summary>
+        /// <returns>Mensaje</returns>
+        private string GetMenssageForBarcodes()
         {
             string message = "Solamente son letras al azar dsjaklsdjaldkjskasdjla";
 
             return message;
         }
+        /// <summary>
+        /// Obtener el Barcore QR
+        /// </summary>
+        /// <param name="text">Mensaje que será pasado a QR</param>
+        /// <returns>Imágen QR</returns>
         private Image GetQr(string text)
         {
-            BarcodeQRCode qrCode = new BarcodeQRCode(text.ToString());
+            BarcodeQRCode qrCode = new BarcodeQRCode(text);
 
             Image qr = new Image(qrCode.CreateFormXObject(null, null));
             qr.Scale(2, 2);
-            qr.SetHorizontalAlignment(HorizontalAlignment.CENTER);
+            qr.SetHorizontalAlignment(HorizontalAlignment.LEFT);
             qr.SetMarginLeft(20);
             qr.SetMarginTop(10);
 
             return qr;
+        }
+        /// <summary>
+        /// Generar el Barcode 25
+        /// </summary>
+        /// <param name="text">Mensaje que será pasado a 25</param>
+        /// <param name="pdfDoc">PdfDocument del documento original</param>
+        /// <returns>Retornar el barcode en una imágen</returns>
+        private Image GetBarcode25(PdfDocument pdfDoc)
+        {
+            BarcodeInter25 barcode25 = new BarcodeInter25(pdfDoc);
+            barcode25.SetCode("0600123456");
+
+            Image barcode = new Image(barcode25.CreateFormXObject(pdfDoc));
+            barcode.Scale(2, 2);
+            barcode.SetHorizontalAlignment(HorizontalAlignment.LEFT);
+            barcode.SetMarginLeft(20);
+            barcode.SetMarginTop(10);
+
+            return barcode;
+        }
+
+        /// <summary>
+        /// Generar el Barcode EAN
+        /// </summary>
+        /// <param name="text">Mensaje que será pasado a 25</param>
+        /// <param name="pdfDoc">PdfDocument del documento original</param>
+        /// <returns>Retornar el EAN en una imágen</returns>
+        private Image GetBarcodeEan(PdfDocument pdfDoc)
+        {
+            BarcodeEAN barcodeEan = new BarcodeEAN(pdfDoc);
+            barcodeEan.SetCode("0123456789012");
+
+            Image barcode = new Image(barcodeEan.CreateFormXObject(null, null, pdfDoc));
+            barcode.Scale(2, 2);
+            barcode.SetHorizontalAlignment(HorizontalAlignment.LEFT);
+            barcode.SetMarginLeft(20);
+            barcode.SetMarginTop(10);
+
+            return barcode;
         }
         #endregion
     }
